c00793440154dce39bf50f40aa7db4e2df37b0f2
mbalbino1997 <rmassimo.balbino@gmail.com>
2025-01-08 20:35:49 +0100

    add

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,5 +1,11 @@
+const connection = require("../data/db");
 function index(req, res) {
+    const sql = "SELECT * FROM movies";
 
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: "Database query failed" });
+        res.json(results);
+    });
 }
 
 function show(req, res) {
